57f3bda98d312664e50d164f2e4592da3754e244
Jean Acosta <jpacostatomaylla@gmail.com>
2024-03-23 19:13:23 -0500

    updated multiselect component

--- resources/js/Pages/People/CreateEdit.vue
+++ resources/js/Pages/People/CreateEdit.vue
@@ -291,7 +291,7 @@ defineExpose({
                 <div class="sm:flex w-full items-center justify-between">
                     <InputLabel for="persontype" value="Tipo"></InputLabel>
                     <div class="sm:w-3/4">
-                        <MultiSelect class="w-full" :options="person_types" v-model:model-value="form.persontype"></MultiSelect>
+                        <MultiSelect class="w-full" :options="person_types" label="descripcion" v-model="form.persontype"></MultiSelect>
 
                         <InputError
                             :message="form.errors.persontype"
